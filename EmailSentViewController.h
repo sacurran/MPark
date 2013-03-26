f17850d8a4f8778c5980e2c4f855d7d3985505f5
marissa <marissbe@umich.edu>
2013-03-25 21:50:39 -0400

    Added My screens email sent and payment confirmes

---
+++ EmailSentViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Email Sent
+//
+//  Created by Marissa Belcher on 3/13/13.
+//  Copyright (c) 2013 Marissa Belcher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
---
+++ EmailSentViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Email Sent
+//
+//  Created by Marissa Belcher on 3/13/13.
+//  Copyright (c) 2013 Marissa Belcher. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
