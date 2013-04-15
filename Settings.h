92c11d071a8581847b0962724d19eb02059fa7f5
Mitchell Thumme <thumme@umich.edu>
2013-04-15 15:25:29 -0400

    My settings screens
    Finally!

---
+++ Settings.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MitchellPark
+//
+//  Created by Mitchell on 3/13/13.
+//  Copyright (c) 2013 Mitchell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Settings : UIViewController
+
+@end
---
+++ Settings.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MitchellPark
+//
+//  Created by Mitchell on 3/13/13.
+//  Copyright (c) 2013 Mitchell. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface Settings ()
+
+@end
+
+@implementation Settings
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
