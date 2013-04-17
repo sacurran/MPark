0eaab73bfb9c473eb31ba171a6311e4dafdd181e
Mitchell Thumme <thumme@umich.edu>
2013-04-17 12:14:10 -0400

    Forwarding Class
    Has get email and charge CC

---
+++ Forwarding.h
@@ -0,0 +1,13 @@
+//
+//  Forwarding.h
+//  MitchellPark
+//
+//  Created by Mitchell on 4/17/13.
+//  Copyright (c) 2013 Mitchell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Forwarding : UIViewController
+
+@end
---
+++ Forwarding.m
@@ -0,0 +1,23 @@
+//
+//  Forwarding.m
+//  MitchellPark
+//
+//  Created by Mitchell on 4/17/13.
+//  Copyright (c) 2013 Mitchell. All rights reserved.
+//
+
+#import "Forwarding.h"
+
+@implementation Forwarding
+
+- static string GetEmail() {
+    NSString *emailadd;
+    return emailadd;
+}
+
+- (void)ChargeCC{
+    //do something here
+}
+
+@end
+
