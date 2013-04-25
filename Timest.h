3d01f37d45c5f2ee842c6c818470ba9ab2a7b862
Mitchell Thumme <thumme@umich.edu>
2013-04-25 17:54:19 -0400

    Timer Functionality
    Added the two timer functions.  Timest.h and Timest.m.  Let me know
    what else we need if this isnt what you like.

---
+++ Timest.h
@@ -0,0 +1,13 @@
+//
+//  Timest.h
+//  Timing
+//
+//  Created by Mitchell on 4/25/13.
+//  Copyright (c) 2013 Mitchell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Timest : UIViewController
+
+@end
